8f97459b4bbb5e55ec40513828b7aa0d819d59e2
rajicroman1-crypto <rajicroman1@gmail.com>
2026-09-21 13:22:26 +0200

    Add files via upload

---
+++ LV2_b.cpp
@@ -0,0 +1,28 @@
+﻿/* LV2 – Zadatak: Pravo na popust Učenik/učenici: Roman Rajič
+ Funkcija prima: int, bool, double Funkcija vraća: bool
+ Moj test – ulaz: 11, 29.99, 1  očekivano: NE provjerava: NE
+ AI: nisam koristio */
+
+#include <iostream>
+using namespace std;
+bool popust(int bod, double racun, bool koristeno) {
+    if (bod >= 12 && racun >= 30 && !koristeno) {
+        return true;
+    }return false;
+}
+int main()
+{
+    int bod;
+    double racun;
+    bool koristeno;
+    cin >> bod;
+    cin >> racun;
+    cin >> koristeno;
+    if (popust(bod, racun, koristeno)) {
+        cout << "DA";
+    }
+    else {
+        cout << "NE";
+    }
+    return 0;
+}
